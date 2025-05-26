bad70b656cfa17db6644ae4ae157f01a23cd0aee
NimraT0409 <zt79491@gmail.com>
2025-05-26 19:07:27 +0500

    Update authController.js

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -220,7 +220,7 @@ export const sendOtp = async (req, res, next) => {
     const emailDetails = {
       from: `TwinkConnect <${process.env.MAIL_USER}>`,
       to: user.email,
-      subject: "TwinkConnect - Here's your OTP",
+      subject: "Resolvix-ticketing - Here's your OTP",
       html: otp(user.firstName, new_otp),
     };
 
@@ -373,7 +373,7 @@ export const forgotPassword = async (req, res, next) => {
     const emailDetails = {
       from: `TwinkConnect <${process.env.MAIL_USER}>`,
       to: user.email,
-      subject: "TwinkConnect - Here's your Password Reset Link",
+      subject: "Resolvix-ticketing - Here's your Password Reset Link",
       html: reset(user.firstName, resetURL),
     };
 
